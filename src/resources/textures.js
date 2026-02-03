3e0afbe0e63de6be10d72d616aecdd13ae49e674
KYSgh <162289300+KYSgh@users.noreply.github.com>
2026-02-03 11:53:39 +0900

    Add files via upload

---
+++ src/resources/textures.js
@@ -0,0 +1,62 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = '../src/jsm/zootopic.gif'
+billboardTextures.bagHolderBetsTexture = '../src/jsm/Bagholdersbetsbillboard.png';
+billboardTextures.homeSweetHomeTexture = '../src/jsm/home-sweet-home-portrait.png';
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.twitter = '../src/jsm/twitter.png';
+boxTexture.LinkedIn = '../src/jsm/linkedInLogo.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.globe = '../src/jsm/thunder.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+boxTexture.writing = '../src/jsm/writing.png';
+
+//material textures
+let stoneTexture = '../src/jsm/stone.png';
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+inputText.activities = '../src/jsm/activities_text.png';
+inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+//OMU
+inputText.OMU_txt = "../src/jsm/OMU-txt.png"; 
+inputText.homeSweetHomeText = '../src/jsm/kansai-text.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+//神戸
+inputText.神戸 = '../src/jsm/神戸.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+// terp - zoo 
+URL.terpsolutions = 'https://velog.io/@kys/%ED%81%AC%EB%A1%A4%EB%A7%81%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EB%89%B4%EC%8A%A4-%EA%B5%AC%EB%8F%85-%EC%9B%B9-%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%A0%9C%EC%9E%91';
+//
+
+URL.bagholderBets = 'https://bagholder-bets.herokuapp.com';
+URL.homeSweetHomeURL = 'https://velog.io/@kys/%EA%B0%84%EC%82%AC%EC%9D%B4-%EA%B3%B5%ED%95%AD-%E9%96%A2%E8%A5%BF%E7%A9%BA%E6%B8%AF%E3%80%80%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88'
+
+//호스팅 GITHUB 링크로 KYS수정S2
+URL.gitHub = 'https://github.com/0xFloyd/Portfolio_2020';
+//벨로그임
+URL.twitter = 'https://velog.io/@kys/posts';
+//링크드인임
+URL.email = 'https://www.linkedin.com/in/ys-k-38242831a/';
+//velogリンク
+URL.githubBagholder = 'https://velog.io/@kys/%EC%96%BC%EA%B5%B4-%EB%AA%AC%EC%8A%A4%ED%84%B0'
+//velogリンク
+URL.githubHomeSweetHome = 'https://velog.io/@kys/%EA%B0%84%EC%82%AC%EC%9D%B4-%EA%B3%B5%ED%95%AD-%E9%96%A2%E8%A5%BF%E7%A9%BA%E6%B8%AF%E3%80%80%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88'
+URL.devTo = 'https://dev.to/0xfloyd/create-an-interactive-3d-portfolio-website-that-stands-out-to-employers-47gc';
+
+export { billboardTextures, boxTexture, inputText, URL, stoneTexture, woodTexture };
